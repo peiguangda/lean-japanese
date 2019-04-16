55ccf40a1212e82784c9521cb248d00cb2f0c1ac
peiguangda <peiguangda@gmail.com>
2019-04-16 22:40:00 +0700

    “heroku”

--- src/common/constants/config.ts
+++ src/common/constants/config.ts
@@ -1,5 +1,5 @@
 export const apiConfig = {
-    'url': 'http://localhost:8080',
+    'url': 'http://0.0.0.0:8080',
     'url-api': 'https://easy-japanese-api-peiguangda.c9users.io',
     'version': '1.0',
 };
